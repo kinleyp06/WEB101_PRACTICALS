789f85a0fa4f96a4fcda8fe72b312efcc7080583
kinleyp06 <kinleypem06@gmail.com>
2026-03-09 14:30:13 +0600

    first commit

---
+++ tiktok/src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 classname="text-4xl font-bold">Tiktok</h1>
+      <p className="mt-4 text-xl">Welcome to our Tiktok project!</p>
+    </main>
+  );
+}
